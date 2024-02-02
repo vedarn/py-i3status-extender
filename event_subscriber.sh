0a5c8f7adcaf4968d2d3af95a60b08ac913e313d
vedarn <35603821+vedarn@users.noreply.github.com>
2024-02-02 13:55:49 +0200

    Filter also title events. This will make window title to be changed immediately if focused app is the same (i.e. opening different websites in a browwser).

--- event_subscriber.sh
+++ event_subscriber.sh
@@ -5,5 +5,5 @@
 if [ "${XDG_SESSION_DESKTOP}" = "i3" ]; then
     i3-msg -t subscribe -m '["window"]' | jq -crM --unbuffered 'select( .change == "focus" )'
 else
-    swaymsg -t subscribe -m '["window", "input"]' | jq -crM --unbuffered 'select( .input.identifier == "1:1:AT_Translated_Set_2_keyboard" or .change == "focus" )'
+    swaymsg -t subscribe -m '["window", "input"]' | jq -crM --unbuffered 'select( .input.identifier == "1:1:AT_Translated_Set_2_keyboard" or .change == "focus" or .change == "title")'
 fi
